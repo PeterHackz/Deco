5f782c7ba7d1844cc18e5f7725d6919038d142c8
SB-9838 <86655602+SB-9838@users.noreply.github.com>
2021-08-13 10:11:13 +0300

    Create invite.js

---
+++ src/commands/general/invite.js
@@ -0,0 +1,10 @@
+//replace deco bot ID with your bot ID
+module.exports = {
+    name: 'invite', 
+    code: `
+$title[BOT invitation] 
+$description[[CLICK-HERE](https://discord.com/oauth2/authorize?client_id=820706261972549682&scope=bot&permissions=379968) to invite me to your server!] 
+$color[RANDOM] 
+$footer[Requested by $username] 
+`
+   } 
